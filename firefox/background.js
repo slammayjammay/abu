b847cecac72fe0ff971c6d1c7818a84afae0e9ed
Scott McKenzie <slammayjammay@gmail.com>
2026-03-07 15:35:31 -0800

    Add background storage access

---
+++ firefox/background.js
@@ -0,0 +1,9 @@
+browser.runtime.onUserScriptMessage.addListener((message, _, respond) => {
+	if (message.action === 'storage:get') {
+		browser.storage.local.get(message.args, data => respond(data));
+		return true;
+	} else if (message.action === 'storage:set') {
+		browser.storage.local.set(message.args, () => respond(true));
+		return true;
+	}
+});
