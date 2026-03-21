56522af1523d2d1d590707c2e75254e4b4766b08
Scott McKenzie <slammayjammay@gmail.com>
2026-03-21 16:07:48 -0700

    Allow emailing

--- firefox/background.js
+++ firefox/background.js
@@ -8,5 +8,27 @@ browser.runtime.onUserScriptMessage.addListener((message, _, respond) => {
 	} else if (message.action === 'notifications:create') {
 		browser.notifications?.create(message.args, (...args) => respond(...args));
 		return true;
+	} else if (message.action === 'email') {
+		message.args && email(message.args).then(success => respond(success));
+		return true;
 	}
 });
+
+async function email({ to, subject, body } = {}) {
+	console.log(`Emailing "${to}"...`);
+
+	const URL = 'https://backend.violetmetal.xyz/api/users/1/emails/send-now';
+	const res = await fetch(URL, {
+		method: 'post',
+		credentials: 'include',
+		body: JSON.stringify({ model: { to, subject, body } }),
+		headers: {
+			'Content-Type': 'application/json',
+			'Accept': 'application/json'
+		},
+	});
+
+	console.log(`Status: "${res.status}".`);
+
+	return res.status === 200;
+}
